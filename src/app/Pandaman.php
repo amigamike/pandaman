0c1bbf99f514d75bb2a267930f0e4b582da69294
Mike Welsh <mike@amigamike.com>
2019-11-22 21:43:57 +0000

    init

---
+++ src/app/Pandaman.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Pandaman.
+ *
+ * @package     AmigaMike\Pandaman\Pandaman
+ * @author      Mike Welsh (mike@amigamike.com)
+ * @copyright   2019 AmigaMike (amigamike.com)
+ * @link        https://amigamike.com
+ * @version     0.0.1
+ */
+namespace AmigaMike\Pandaman\Pandaman;
+
+class Pandaman
+{
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ src/config/slack.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Slack config.
+ *
+ * @author      Mike Welsh (mike@amigamike.com)
+ * @copyright   2019 AmigaMike (amigamike.com)
+ * @link        https://amigamike.com
+ * @version     0.0.1
+ */
+$config['slack'] = [
+
+];
